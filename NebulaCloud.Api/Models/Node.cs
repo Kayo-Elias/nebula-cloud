ec52f277b2723813850f7e52f50edb94063d087b
Kayo Elias <eliaskayo579@gmail.com>
2026-08-21 11:48:45 -0300

    feat: implement node status

--- NebulaCloud.Api/Models/Node.cs
+++ NebulaCloud.Api/Models/Node.cs
@@ -4,7 +4,16 @@ public class Node
 {
     public IPAddress Ip { get; set; }
     public string Name { get; set;}
-    public bool Status { get; set; }
+    public NodeStatus Status { get; set; }
     public double Memory { get; set;}
     public double Storage { get; set; }
+}
+
+public enum NodeStatus
+{
+    Offline,
+    Online,
+    Maintenance,
+    Provisioning,
+    Error
 }
