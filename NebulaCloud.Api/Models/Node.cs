1ddfc5412eb1116488b455fbd431693047aacdfe
Kayo Elias <eliaskayo579@gmail.com>
2026-08-21 11:22:13 -0300

    feat: add initial node model

---
+++ NebulaCloud.Api/Models/Node.cs
@@ -0,0 +1,10 @@
+using System.Net;
+
+public class Node
+{
+    public IPAddress Ip { get; set; }
+    public string Name { get; set;}
+    public bool Status { get; set; }
+    public double Memory { get; set;}
+    public double Storage { get; set; }
+}
