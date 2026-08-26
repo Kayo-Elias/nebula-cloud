be7c1ee061895b541b1c0d5cd25d45b6a56f7154
Kayo Elias <eliaskayo579@gmail.com>
2026-08-26 13:31:20 -0300

    feat: add Node.Tests project

---
+++ Node.Tests/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Node.Tests;
+
+public class NodeNameValidatorTests
+{
+    [Fact]
+    public void Should_Return_True_When_Name_Is_Valid()
+    {
+        var node = new Node
+        {
+            Name = "Server01"
+        };
+
+        var validator = new NodeNameValidator();
+
+        var result = validator.IsValid(node);
+
+        Assert.True(result);
+    }
+}
