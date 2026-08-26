1d1f588787be506d428ce00691428bf51a560d1a
Kayo Elias <eliaskayo579@gmail.com>
2026-08-26 10:00:55 -0300

    feat(NodeNameValidator): implements regex for valid node characters

--- NebulaCloud.Api/Models/Node/NodeNameValidator.cs
+++ NebulaCloud.Api/Models/Node/NodeNameValidator.cs
@@ -6,15 +6,20 @@ public class NodeNameValidator
 {
     public bool IsValid(Node node)
     {
-        if(node == null || string.IsNullOrWhiteSpace(node.Name))
+        if (node == null || string.IsNullOrWhiteSpace(node.Name))
         {
             return false;
         }
-            return true;
+        return true;
     }
 
     public bool HasMaximumLength(Node node)
     {
         return node.Name.Length <= 15;
+    }
+
+    public bool HasValidCharacters(Node node)
+    {
+        return Regex.IsMatch(node.Name, @"^[a-zA-Z0-9_-]+$");
     }
 }
