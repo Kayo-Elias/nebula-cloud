8d563e5b6e741e87016b40e9d410c9aec0dd69f1
Kayo Elias <eliaskayo579@gmail.com>
2026-08-28 10:25:30 -0300

    refactor: encapsulate node properties

--- NebulaCloud.Api/Models/Node/Node.cs
+++ NebulaCloud.Api/Models/Node/Node.cs
@@ -4,11 +4,18 @@ namespace Node;
 
 public class Node
 {
-    public IPAddress Ip { get; set; }
-    public string Name { get; set;}
-    public NodeStatus Status { get; set; }
-    public double Memory { get; set;}
-    public double Storage { get; set; }
+    public IPAddress? Ip { get; private set; }
+    public string? Name { get; private set;}
+    public NodeStatus Status { get; private set; }
+    public double Memory { get; private set;}
+    public double Storage { get; private set; }
+
+    public Node() { }
+    public Node (string name, IPAddress ip)
+    {
+        Name = name;
+        Ip = ip;
+    } 
 }
 
 public enum NodeStatus
